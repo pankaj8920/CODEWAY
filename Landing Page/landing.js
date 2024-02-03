09eb9737556d7a788f9a172082119874861ff3e9
pankaj8920 <158765718+pankaj8920@users.noreply.github.com>
2024-02-04 01:20:30 +0530

    Second Project

---
+++ Landing Page/landing.js
@@ -0,0 +1,44 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".header__container h1", {
+    ...scrollRevealOption,
+  });
+  
+  ScrollReveal().reveal(".header__form", {
+    ...scrollRevealOption,
+    delay: 500,
+  });
+  
+
+  ScrollReveal().reveal(".trending__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+
+  ScrollReveal().reveal(".destination__card", {
+    duration: 1000,
+    interval: 500,
+  });
+  
+
+  ScrollReveal().reveal(".seller__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+  
+  ScrollReveal().reveal(".guide__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
+  
+ 
+  ScrollReveal().reveal(".client__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
