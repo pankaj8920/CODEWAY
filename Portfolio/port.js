95a685b37cea04add8d6cae65b181885f9450ba8
pankaj8920 <158765718+pankaj8920@users.noreply.github.com>
2024-02-14 22:38:00 +0530

    Portfolio website

---
+++ Portfolio/port.js
@@ -0,0 +1,11 @@
+var tablinks = Document.getElementsByClassName("tab-links");
+var tabcontents = Document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+}
